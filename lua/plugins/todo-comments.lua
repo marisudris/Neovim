7f0fe2b8fcc3138c7c59781470705ca331a2cba2
Maris Udris <maris.udris.lv@gmail.com>
2026-04-14 12:03:34 +0000

    Add todo-comments plugin and make it play nice with the theme

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,30 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = function()
+    local p = require("rose-pine.palette")
+
+    return {
+      signs = true,
+
+      -- Mapping Folke's color names to Rose Pine palette
+      colors = {
+        error   = { p.love },    -- Red/Pink
+        warning = { p.gold },    -- Yellow/Gold
+        info    = { p.pine },    -- Teal/Pine
+        hint    = { p.foam },    -- Cyan/Foam
+        default = { p.iris },    -- Purple
+        test    = { p.rose },    -- Rose/Pink
+      },
+      gui_style = {
+        fg = "NONE",
+        bg = "BOLD",
+      },
+      highlight = {
+        keyword = "wide",
+        after = "fg",
+        comments_only = true,
+      },
+    }
+  end
+}
