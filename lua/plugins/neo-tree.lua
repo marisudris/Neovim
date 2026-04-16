dd3946f1e109ff063c2f70169a600671ad55bd3c
Maris Udris <maris.udris.lv@gmail.com>
2026-04-16 12:27:55 +0000

    Add neotree file explorer

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    cmd = "Neotree",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    lazy = false,
+  },
+}
