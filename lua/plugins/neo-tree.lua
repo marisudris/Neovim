cf4bbf04e9157b400a98c4013f01881c46915756
Maris Udris <maris.udris.lv@gmail.com>
2026-04-16 15:35:08 +0000

    Add keybindings for neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,5 +9,61 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     lazy = false,
+    keys = {
+      {
+        "<leader>e",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+        end,
+        desc = "Toggle Explorer (cwd)",
+      },
+      {
+        "<leader>o",
+        function()
+          require("neo-tree.command").execute({ toggle = true, reveal = true })
+        end,
+        desc = "Reveal current file",
+      },
+      {
+        "<leader>ge",
+        function()
+          require("neo-tree.command").execute({ source = "git_status", toggle = true })
+        end,
+        desc = "Git Explorer",
+      },
+    },
+
+    config = function()
+      require("neo-tree").setup({
+        close_if_last_window = true,
+        popup_border_style = "rounded",
+        enable_git_status = true,
+        enable_diagnostics = true,
+
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          },
+          follow_current_file = {
+            enabled = true,
+          },
+          use_libuv_file_watcher = true,
+        },
+
+        window = {
+          width = 30,
+          mappings = {
+            ["<space>"] = "toggle_node",
+            ["l"] = "open",
+            ["h"] = "close_node",
+            ["<cr>"] = "open",
+            ["s"] = "open_split",
+            ["v"] = "open_vsplit",
+            ["R"] = "refresh",
+          },
+        },
+      })
+    end,
   },
 }
